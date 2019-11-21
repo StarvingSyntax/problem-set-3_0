e16af7c0450e75b13cccb2e55271a6c84a18c96d
EC2 Default User <ec2-user@ip-172-31-32-161.us-east-2.compute.internal>
2019-11-21 16:38:04 +0000

    3.0

--- exercises.js
+++ exercises.js
@@ -1,4 +1,33 @@
+const swap = (str) => {
+  let strArr = str.split(' ');
+  let result = strArr.map(word => {
+    if (word.length === 1) {
+      return word;
+    }
+    return word[word.length - 1] + word.substring(1,word.length - 1) + word[0];
+  }); 
+  return result.join(' ');
+};
 
+const multiplyList = (arr1, arr2) => {
+  const newArray = new Array(arr1.length); //create empty array.
+  newArray.fill(0, 0, newArray.length)
+
+  return newArray.map((_, index) => {
+    return arr1[index] * arr2[index];
+  })
+}
+
+const sequence = (integer) => {
+  const preNumIterate = new Array(integer);
+    preNumIterate.fill(0, 0, integer)
+  // preNumIterate[integer - 1] =  integer;
+
+  preNumIterate.map((value, index) => {
+    preNumIterate[index] = index + 1;
+  });
+  console.log(preNumIterate)
+}
 
 // The below code connects this JS fil to our test file
 module.exports = {
